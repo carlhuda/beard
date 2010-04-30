cf1bc40a34c662de66efd577d29eee660de906af
wycats <wycats@gmail.com>
2010-04-30 16:16:02 -0700

    Add Haml

--- lib/beard.rb
+++ lib/beard.rb
@@ -2,3 +2,9 @@ require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'dm-rails'
 require 'rspec-rails'
+
+module Beard
+  class Railtie < ::Rails::Railtie
+    config.generators.template_engine :haml
+  end
+end
