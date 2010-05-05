914628926f18814d896d477b66647f0d6776bb61
wycats <wycats@gmail.com>
2010-05-05 06:20:18 +0200

    Hide namespaces for generators we don't want

--- lib/generators/beard/generator.rb
+++ lib/generators/beard/generator.rb
@@ -1,3 +1,16 @@
+# Hide the namespaces for Rails generators we aren't using,
+# as well as generators that we already used during the
+# install process
+Rails::Generators.hide_namespaces(
+  :erb,
+  :test_unit,
+  :beard,
+  :devise,
+  :devise_install,
+  :devise_views,
+  "rspec:install"
+)
+
 module Beard
   module Generator
     def source_root
